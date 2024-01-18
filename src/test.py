2dae4ba5f993881dfb1294c0bbf9fc8676a99da3
Michael Mayr <mayr.michael@outlook.com>
2024-01-18 22:55:29 +0100

    test yi

--- src/test.py
+++ src/test.py
@@ -63,7 +63,7 @@ async def cosim_tt_sin(dut):
     
     dut.enable.value = 1
 
-    forked = cocotb.start_soon(print_test_value(dut, calc_values_xi))
+    forked = cocotb.start_soon(print_test_value(dut, calc_values_yi))
     # await cocotb.start_soon(enable_control(dut))
 
     await Join(forked)
